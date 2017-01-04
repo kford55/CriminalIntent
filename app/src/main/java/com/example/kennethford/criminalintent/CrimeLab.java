122b2a0e419550fbb7d298c609f02fdbf98a9fc5
Ford <Kenneth.Ford@capitalone.com>
2017-01-04 15:49:53 -0500

    Added base sqllite classes

--- app/src/main/java/com/example/kennethford/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/kennethford/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.example.kennethford.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.kennethford.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,8 @@ public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -19,14 +24,9 @@ public class CrimeLab {
     }
 
     private CrimeLab (Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
     public List<Crime> getCrimes() {
